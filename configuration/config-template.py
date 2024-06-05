49ab82ea4bf63100e9977eac0fc801fc9534dc6b
Katia Lopes-Gilbert <katialopesgilbert@gmail.com>
2024-06-04 20:41:06 -0700

    Resolved conflict.

--- configuration/config-template.py
+++ configuration/config-template.py
@@ -1,6 +1,2 @@
 IMAGE_PATH='your/image/path/here'
-<<<<<<< HEAD:config-template.py
 DATA_PATH='your/data/path/here'
-=======
-DATA_PATH='you/data/path/here'
->>>>>>> 86323f21d95af0a61929137460017cda30854e67:configuration/config-template.py
